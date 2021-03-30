b657f7097b454fd77aba986f171b9df801942eab
Zack Krida <zackkrida@pm.me>
2021-03-30 16:32:07 -0400

    prettier time

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -3,84 +3,80 @@ import ErrorPage from 'next/error'
 import Head from 'next/head'
 import { getPostBySlug, getAllPosts } from 'lib/api'
 import markdownToHtml from 'lib/markdownToHtml'
-import {format} from 'date-fns'
+import { format } from 'date-fns'
 
 export default function Post({ post, morePosts, preview }) {
-  const router = useRouter()
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
-  }
-  return (
-      <div>
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article>
-              <Head>
-                <title>
-                  {post.title} ~ zack.cat
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
-              </Head>
+	const router = useRouter()
+	if (!router.isFallback && !post?.slug) {
+		return <ErrorPage statusCode={404} />
+	}
+	return (
+		<div>
+			{router.isFallback ? (
+				<PostTitle>Loading…</PostTitle>
+			) : (
+				<>
+					<article>
+						<Head>
+							<title>{post.title} ~ zack.cat</title>
+							<meta property="og:image" content={post.ogImage.url} />
+						</Head>
 
-              <div>
-                <h1>
-                  {post.title}
-                </h1>
-                {/* <p>
+						<div>
+							<h1>{post.title}</h1>
+							{/* <p>
                   {format(new Date(post.date), 'M/d/y')}
                 </p>
                 <p>
                   {post.author.name}
                 </p> */}
-                {/* {JSON.stringify(post.coverImage, null, 2)}<br /> */}
-                {/* {JSON.stringify(post.author, null, 2)}<br /> */}
+							{/* {JSON.stringify(post.coverImage, null, 2)}<br /> */}
+							{/* {JSON.stringify(post.author, null, 2)}<br /> */}
 
-                <div dangerouslySetInnerHTML={{__html: post.content }} />
-              </div>
-            </article>
-          </>
-        )}
-      </div>
-  )
+							<div dangerouslySetInnerHTML={{ __html: post.content }} />
+						</div>
+					</article>
+				</>
+			)}
+		</div>
+	)
 }
 
 export async function getStaticProps({ params }) {
-  const slug = Array.isArray(params.slug) ? params.slug[0] : 'index' // special case for homepage
-  const post = getPostBySlug(slug || 'index', [
-    'title',
-    'date',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
-    'coverImage',
-  ])
-  const content = await markdownToHtml(post.content || '')
+	const slug = Array.isArray(params.slug) ? params.slug[0] : 'index' // special case for homepage
+	const post = getPostBySlug(slug || 'index', [
+		'title',
+		'date',
+		'slug',
+		'author',
+		'content',
+		'ogImage',
+		'coverImage',
+	])
+	const content = await markdownToHtml(post.content || '')
 
-  return {
-    props: {
-      post: {
-        ...post,
-        content,
-      },
-    },
-  }
+	return {
+		props: {
+			post: {
+				...post,
+				content,
+			},
+		},
+	}
 }
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(['slug'])
-  const paths = posts.map((post) => {
-      return {
-        params: {
-          slug: [post.slug === 'index' ? '' : post.slug],
-        },
-      }
-    })
+	const posts = getAllPosts(['slug'])
+	const paths = posts.map((post) => {
+		return {
+			params: {
+				slug: [post.slug === 'index' ? '' : post.slug],
+			},
+		}
+	})
 
-  return {
-    paths,
-    fallback: false,
-  }
+	return {
+		paths,
+		fallback: false,
+	}
 }
